4993807cd1af1d5c91f93f1b90c486a7197ad56f
albertlatacz <albert.latacz@gmail.com>
2023-01-26 22:09:02 +0000

    jenv setup

--- brew/setup.sh
+++ brew/setup.sh
@@ -7,7 +7,7 @@ echo Brew setup
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 BREW_COMMAND='eval "$(/opt/homebrew/bin/brew shellenv)"'
-if [[ $(grep -L $BREW_COMMAND ~/.zprofile) ]]; then
+if [[ $(grep -L "$BREW_COMMAND" ~/.zprofile) ]]; then
   echo '# Set PATH, MANPATH, etc., for Homebrew.' >> ~/.zprofile
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 fi
@@ -18,7 +18,7 @@ brew tap Homebrew/bundle
 brew bundle
 
 JENV_COMMAND='export PATH="$HOME/.jenv/bin:$PATH"'
-if [[ $(grep -L $BREW_COMMAND ~/.zprofile) ]]; then
+if [[ $(grep -L "$JENV_COMMAND" ~/.zprofile) ]]; then
   echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.zprofile
   echo 'eval "$(jenv init -)"' >> ~/.zprofile
 fi
