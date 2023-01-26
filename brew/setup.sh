c469eb22fd7a75fba8a9242a863a6aa99f82616f
albertlatacz <albert.latacz@gmail.com>
2023-01-26 21:33:50 +0000

    brew

--- brew/setup.sh
+++ brew/setup.sh
@@ -5,6 +5,11 @@ set -e
 echo Brew setup
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/albert/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/albert/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew tap Homebrew/bundle
 brew bundle
 
